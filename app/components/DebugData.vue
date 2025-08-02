b5a7677fb531c41afeb880f626b605c7e1c3159b
Christian Cueni <christian.cueni@iterativ.ch>
2025-08-02 20:43:19 +0200

    Use valid html

--- app/components/DebugData.vue
+++ app/components/DebugData.vue
@@ -10,21 +10,46 @@ const showData = ref(false);
 </script>
 
 <template>
-  <Button class="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-sky-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-  @click="showData = !showData">
+  <Button
+    class="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-sky-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+    @click="showData = !showData">
     <Eye v-if="!showData" />
     <EyeOff v-else />
     Show Data
   </Button>
   <div v-if="showData">
-    <p>Current Playback Time: {{ store.videoCurrentTime.toFixed(2) }}</p>
-    <p>Video Duration: {{ store.videoDuration.toFixed(2) }} seconds</p>
-    <p>Current GPS Data:<pre>{{ store.currentGpsData }}</pre></p>
-    <p>Current Acceleration Data:<pre>{{ store.currentAccelerationData }}</pre></p>
-    <p>Current Face Data:<pre>{{ store.currentFaceData }}</pre></p>
-    <p>Current Luminance Data:<pre>{{ store.currentLuminanceData }}</pre></p>
-    <p>Current Hue Data:<pre>{{ store.currentHueData }}</pre></p>
-    <p>Current Scene Data:<pre>{{ store.currentSceneData }}</pre></p>
+    <div class="flex flex-col gap-1 mb-4">
+      <h4>Current Playback Time</h4>
+      <p>{{ store.videoCurrentTime.toFixed(2) }}</p>
+    </div>
+    <div class="flex flex-col gap-1 mb-4">
+      <h4>Video Duration</h4>
+      <p>{{ store.videoDuration.toFixed(2) }} seconds</p>
+    </div>
+    <div class="flex flex-col gap-1 mb-4">
+      <h4>Current GPS Data</h4>
+      <pre>{{ store.currentGpsData }}</pre>
+    </div>
+    <div class="flex flex-col gap-1 mb-4">
+      <h4>Current Acceleration Data</h4>
+      <pre>{{ store.currentAccelerationData }}</pre>
+    </div>
+    <div class="flex flex-col gap-1 mb-4">
+      <h4>Current Face Data</h4>
+      <pre>{{ store.currentFaceData }}</pre>
+    </div>
+    <div class="flex flex-col gap-1 mb-4">
+      <h4>Current Luminance Data</h4>
+      <pre>{{ store.currentLuminanceData }}</pre>
+    </div>
+    <div class="flex flex-col gap-1 mb-4">
+      <h4>Current Hue Data</h4>
+      <pre>{{ store.currentHueData }}</pre>
+    </div>
+    <div class="flex flex-col gap-1 mb-4">
+      <h4>Current Scene Data</h4>
+      <pre>{{ store.currentSceneData }}</pre>
+    </div>
   </div>
 </template>
 
@@ -39,4 +64,3 @@ const showData = ref(false);
   opacity: 0;
 }
 </style>
-
